0fad2f39f59b3f9d4f859fa1acad35a93fdc6440
Vasu Arora <shrisw2002@gmail.com>
2024-09-02 14:26:35 +0530

    Initial commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      This is an about component.
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem, amet!
+        Amet non eveniet esse rerum numquam atque libero dolores fugit maxime,
+        fuga deserunt exercitationem necessitatibus suscipit aperiam cum natus
+        iusto unde nemo reiciendis consectetur.      
+      </p>
+    </div>
+  );
+};
+
+export default About;
